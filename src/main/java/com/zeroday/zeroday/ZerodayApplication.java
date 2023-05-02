bed319ed3ddb4bc9a4a9c057bc9a35c0b6aa9c40
ezequieljm <ezequieljm020@gmail.com>
2023-05-01 23:33:46 -0300

    Initial commit

---
+++ src/main/java/com/zeroday/zeroday/ZerodayApplication.java
@@ -0,0 +1,13 @@
+package com.zeroday.zeroday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeroday/zeroday/ZerodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroday.zeroday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
